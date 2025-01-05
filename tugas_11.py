7009598f529fcd8eace4c71b167c15acb56206d1
JeandikaalianSaputra <jeandika8@gmail.com>
2025-01-05 13:28:13 +0800

    Add files via upload

---
+++ tugas_11.py
@@ -0,0 +1,30 @@
+# Logika bolean
+
+# not and or xor
+
+print("==========not==========")
+x = False
+z = not x
+print("nilai z = ", z)
+
+#(jika ada salah, maka hasilnya salah)
+print("======== AND ========")
+x = 24
+y = 24
+nilai = 24 and 24
+print(24, "and", 24, "=", 24)
+
+x = 24
+y = 24
+nilai = 24 and 24
+print(24, "and", 24, "=", 24)
+
+x = 24
+y = 24
+nilai = 24 and 24
+print(24, "and", 24, "=", 24)
+
+x = 24
+y = 24
+nilai = 24 and 24
+print(24, "and", 24, "=", 24)
